bcf863ba5ad837476b57e994e1268bea557a2156
Gratianus Martin Brianto <martin.gratianus@hotmail.com>
2019-06-13 23:08:09 +0700

    simple galerry 1

---
+++ src/main/java/com/example/simplegallery/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.simplegallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.imagegallery);
+        recyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(),3);
+        recyclerView.setLayoutManager(layoutManager);
+        ArrayList<CreateList> createLists = prepareData();
+        MyAdapter adapter = new MyAdapter(getApplicationContext(), createLists);
+        recyclerView.setAdapter(adapter);
+
+
+    }
+    private final String image_titles[] = {
+            "Img1",
+            "Img2",
+            "Img3",
+            "Img4",
+            "Img5",
+            "Img6",
+            "Img7",
+            "Img8",
+            "Img9",
+            "Img10",
+            "Img11",
+            "Img12",
+            "Img13",
+    };
+
+    private final Integer image_ids[] = {
+            R.drawable.img1,
+            R.drawable.img2,
+            R.drawable.img3,
+            R.drawable.img4,
+            R.drawable.img5,
+            R.drawable.img6,
+            R.drawable.img7,
+            R.drawable.img8,
+            R.drawable.img9,
+            R.drawable.img10,
+            R.drawable.img11,
+            R.drawable.img12,
+            R.drawable.img13,
+
+    };
+
+    private ArrayList<CreateList> prepareData(){
+
+        ArrayList<CreateList> theimage = new ArrayList<>();
+        for(int i = 0; i< image_titles.length; i++){
+            CreateList createList = new CreateList();
+            createList.setImage_title(image_titles[i]);
+            createList.setImage_ID(image_ids[i]);
+            theimage.add(createList);
+        }
+        return theimage;
+    }
+
+
+
+
+}
+
